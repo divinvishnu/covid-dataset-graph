169e82b4261cfe9dad637980820a0b2f7e84f2a1
Divin Vishnu Ramachandran <ramachandrandivinvishnu@johndeere.com>
2021-04-21 20:33:21 -0500

    User can now select total cases or new cases per day graphing

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import pydeck as pdk
+import altair as alt
+
 
 @st.cache(persist=True, suppress_st_warning=True, allow_output_mutation=True)
 def wwConfirmedDataCollection():
@@ -15,74 +17,109 @@ def wwConfirmedDataCollection():
     recovery_df = pd.read_csv(recovery_url)
     return confirmed_df, death_df, recovery_df
 
-def displayRawData(confirmed_df, death_df, recovery_df):    
+
+def displayRawData(confirmed_df, death_df, recovery_df):
     if st.sidebar.checkbox("Display Raw Confirmation Data", False) == True:
         st.write('Data from "CSSEGISandData"')
-        st.write('Confirmed Cases')
+        st.write("Confirmed Cases")
         st.write(confirmed_df)
-        st.write('Death from Cases')
+        st.write("Death from Cases")
         st.write(death_df)
-        st.write('Recovered from Cases')
+        st.write("Recovered from Cases")
         st.write(recovery_df)
 
+
 def dataMassaging(confirmed_df, death_df, recovery_df):
-    #converts the column names to lowercase
+    # converts the column names to lowercase
     confirmed_df = confirmed_df.rename(columns=str.lower)
     death_df = death_df.rename(columns=str.lower)
     recovery_df = recovery_df.rename(columns=str.lower)
 
     dates = confirmed_df.columns[4:]
-    confirmed_df = pd.melt(confirmed_df, id_vars=['country/region', 'lat', 'long'],value_vars=dates,var_name='date',value_name='confirmed')
-    death_df = pd.melt(death_df, id_vars=['country/region', 'lat', 'long'],value_vars=dates,var_name='date',value_name='deaths')
-    recovery_df = pd.melt(recovery_df, id_vars=['country/region', 'lat', 'long'],value_vars=dates,var_name='date',value_name='recovered')
+    confirmed_df = pd.melt(
+        confirmed_df,
+        id_vars=["country/region", "lat", "long"],
+        value_vars=dates,
+        var_name="date",
+        value_name="confirmed",
+    )
+    death_df = pd.melt(
+        death_df,
+        id_vars=["country/region", "lat", "long"],
+        value_vars=dates,
+        var_name="date",
+        value_name="deaths",
+    )
+    recovery_df = pd.melt(
+        recovery_df,
+        id_vars=["country/region", "lat", "long"],
+        value_vars=dates,
+        var_name="date",
+        value_name="recovered",
+    )
 
-    #converting to dates
-    confirmed_df['date'] = pd.to_datetime(confirmed_df['date'])
-    death_df['date'] = pd.to_datetime(death_df['date'])
-    recovery_df['date'] = pd.to_datetime(recovery_df['date'])
+    # converting to dates
+    confirmed_df["date"] = pd.to_datetime(confirmed_df["date"])
+    death_df["date"] = pd.to_datetime(death_df["date"])
+    recovery_df["date"] = pd.to_datetime(recovery_df["date"])
 
-    #Sum of canada confirmed cases, deaths, recovery
-    canada_confirmed = confirmed_df[confirmed_df['country/region'] == 'Canada']
-    canada_death = death_df[death_df['country/region'] == 'Canada']
-    canada_recovery = recovery_df[recovery_df['country/region'] == 'Canada']
+    # Sum of canada confirmed cases, deaths, recovery
+    canada_confirmed = confirmed_df[confirmed_df["country/region"] == "Canada"]
+    canada_death = death_df[death_df["country/region"] == "Canada"]
+    canada_recovery = recovery_df[recovery_df["country/region"] == "Canada"]
 
-    canada_confirmed = canada_confirmed.groupby(['date'])['confirmed'].sum().reset_index()
-    canada_confirmed['country/region'] = 'Canada'
-    canada_confirmed['lat'] = '56.1304'
-    canada_confirmed['long'] = '-106.3468'
-    canada_confirmed = canada_confirmed[['country/region', 'lat', 'long', 'date', 'confirmed']]
+    canada_confirmed = (
+        canada_confirmed.groupby(["date"])["confirmed"].sum().reset_index()
+    )
+    canada_confirmed["country/region"] = "Canada"
+    canada_confirmed["lat"] = "56.1304"
+    canada_confirmed["long"] = "-106.3468"
+    canada_confirmed = canada_confirmed[
+        ["country/region", "lat", "long", "date", "confirmed"]
+    ]
 
-    canada_death = canada_death.groupby(['date'])['deaths'].sum().reset_index()
-    canada_death['country/region'] = 'Canada'
-    canada_death['lat'] = '56.1304'
-    canada_death['long'] = '-106.3468'
-    canada_death = canada_death[['country/region', 'lat', 'long', 'date', 'deaths']]
-    
-    canada_recovery = canada_recovery.groupby(['date'])['recovered'].sum().reset_index()
-    canada_recovery['country/region'] = 'Canada'
-    canada_recovery['lat'] = '56.1304'
-    canada_recovery['long'] = '-106.3468'
-    canada_recovery = canada_recovery[['country/region', 'lat', 'long', 'date', 'recovered']]
-    
+    canada_death = canada_death.groupby(["date"])["deaths"].sum().reset_index()
+    canada_death["country/region"] = "Canada"
+    canada_death["lat"] = "56.1304"
+    canada_death["long"] = "-106.3468"
+    canada_death = canada_death[["country/region", "lat", "long", "date", "deaths"]]
 
-    confirmed_df = confirmed_df[confirmed_df['country/region']!='Canada']
-    death_df = death_df[death_df['country/region']!='Canada']
-    recovery_df = recovery_df[recovery_df['country/region']!='Canada']
+    canada_recovery = canada_recovery.groupby(["date"])["recovered"].sum().reset_index()
+    canada_recovery["country/region"] = "Canada"
+    canada_recovery["lat"] = "56.1304"
+    canada_recovery["long"] = "-106.3468"
+    canada_recovery = canada_recovery[
+        ["country/region", "lat", "long", "date", "recovered"]
+    ]
 
-    
-    new_confirmed_df = confirmed_df.append(canada_confirmed,ignore_index=True)
-    new_confirmed_df.sort_values(['date','country/region'], ascending=[True, True], inplace=True)
+    confirmed_df = confirmed_df[confirmed_df["country/region"] != "Canada"]
+    death_df = death_df[death_df["country/region"] != "Canada"]
+    recovery_df = recovery_df[recovery_df["country/region"] != "Canada"]
+
+    new_confirmed_df = confirmed_df.append(canada_confirmed, ignore_index=True)
+    new_confirmed_df.sort_values(
+        ["date", "country/region"], ascending=[True, True], inplace=True
+    )
     new_confirmed_df = new_confirmed_df.reset_index(drop=True)
 
-    new_death_df = death_df.append(canada_death,ignore_index=True)
-    new_death_df.sort_values(['date','country/region'], ascending=[True, True], inplace=True)
+    new_death_df = death_df.append(canada_death, ignore_index=True)
+    new_death_df.sort_values(
+        ["date", "country/region"], ascending=[True, True], inplace=True
+    )
     new_death_df = new_death_df.reset_index(drop=True)
 
-    new_recovery_df = recovery_df.append(canada_recovery,ignore_index=True)
-    new_recovery_df.sort_values(['date','country/region'], ascending=[True, True], inplace=True)
+    new_recovery_df = recovery_df.append(canada_recovery, ignore_index=True)
+    new_recovery_df.sort_values(
+        ["date", "country/region"], ascending=[True, True], inplace=True
+    )
     new_recovery_df = new_recovery_df.reset_index(drop=True)
 
-    if st.sidebar.checkbox("Display combined join data after canada calculation", False) == True:
+    if (
+        st.sidebar.checkbox(
+            "Display combined join data after canada calculation", False
+        )
+        == True
+    ):
         st.write("JOINING DATA TOGETHER ON DATE")
         st.write("Confirmed")
         st.write(new_confirmed_df)
@@ -93,88 +130,135 @@ def dataMassaging(confirmed_df, death_df, recovery_df):
 
     return new_confirmed_df, new_death_df, new_recovery_df
 
-def rgb(minimum, maximum, value):
-            minimum, maximum = float(minimum), float(maximum)
-            ratio = 2 * (value-minimum) / (maximum - minimum)
-            b = int(max(0, 255*(1 - ratio)))
-            r = int(max(0, 255*(ratio - 1)))
-            g = 255 - b - r
-            return r, g, b
 
-def main():    
+def human_format(num):
+    num = float("{:.3g}".format(num))
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return "{}{}".format(
+        "{:f}".format(num).rstrip("0").rstrip("."), ["", "K", "M", "B", "T"][magnitude]
+    )
+
+
+def main():
     confirmed_df, death_df, recovery_df = wwConfirmedDataCollection()
     st.title("Covid-19 🦠 Pandemic Data Visualization")
     displayRawData(confirmed_df, death_df, recovery_df)
-    confirmed_df, death_df, recovery_df = dataMassaging(confirmed_df, death_df, recovery_df)
-    
-    full_table = confirmed_df.merge(right=death_df, how='left',
-        on=['country/region', 'date', 'lat', 'long'])
-    
-    full_table = full_table.merge(right=recovery_df, how='left',
-        on=['country/region', 'date', 'lat', 'long'])
+    confirmed_df, death_df, recovery_df = dataMassaging(
+        confirmed_df, death_df, recovery_df
+    )
+
+    full_table = confirmed_df.merge(
+        right=death_df, how="left", on=["country/region", "date", "lat", "long"]
+    )
+
+    full_table = full_table.merge(
+        right=recovery_df, how="left", on=["country/region", "date", "lat", "long"]
+    )
 
     st.write('Data from "CSSEGISandData POST data massaging"')
-    full_table = full_table.rename(columns={'long':'lon'})
-    full_table['recovered'] = full_table['recovered'].fillna(0)
-    rows_with_zero_location = full_table['country/region'].str.contains('Diamond Princess') | full_table['country/region'].str.contains('MS Zaandam')
+    full_table = full_table.rename(columns={"long": "lon"})
+    full_table["recovered"] = full_table["recovered"].fillna(0)
+    rows_with_zero_location = full_table["country/region"].str.contains(
+        "Diamond Princess"
+    ) | full_table["country/region"].str.contains("MS Zaandam")
     full_table = full_table[~(rows_with_zero_location)]
     # st.write(full_table.isna().sum())
     # full_table.drop(columns=['province/state'], axis=1)
 
-    # removing the time associated with date_time to just date to help reduce complexity in data. 
-    full_table['date'] = full_table['date'].dt.date
+    # removing the time associated with date_time to just date to help reduce complexity in data.
+    full_table["date"] = full_table["date"].dt.date
 
-    full_table = full_table.rename(columns={'country/region':'location'})
-    user_selectionbox_input = st.selectbox("Select an option", ['Global', 'Select from list of countries'])
-    if user_selectionbox_input == 'Select from list of countries':
-        list_of_countries = full_table['location'].unique()
-        selected_countries = st.multiselect('Select countries', list_of_countries)
+    full_table = full_table.rename(columns={"country/region": "location"})
+    # full_table.insert(
+    #     1,
+    #     "ΔConfirmed",
+    #     full_table.groupby(level=("location", "date"))["Confirmed"]
+    #     .diff()
+    #     .fillna(0)
+    #     .astype(int),
+    # )
 
-        mask_countries = full_table['location'].isin(selected_countries)
+    user_selectionbox_input = st.selectbox(
+        "Select an option", ["Global", "Select from list of countries"]
+    )
+    if user_selectionbox_input == "Select from list of countries":
+        list_of_countries = full_table["location"].unique()
+        selected_country = st.selectbox("Select country", list_of_countries)
+
+        mask_countries = full_table["location"] == (selected_country)
         full_table = full_table[mask_countries]
+
+        # Adding new cases to the table for graphing
+        full_table["new cases"] = full_table["confirmed"].diff(1)
+
+        user_input = st.selectbox(
+            "Select an option", ["Total Number of Cases", "New Cases Per Day"]
+        )
         st.write(full_table)
-        temp = pd.DataFrame(full_table, columns=['date','confirmed','deaths','recovered'])
-        temp = temp.rename(columns={'date':'index'}).set_index('index')
+        if user_input == "New Cases Per Day":
+            temp = pd.DataFrame(full_table, columns=["date", "new cases"])
+            st.write(f"New Cases Per Day for {selected_country}")
+        else:
+            temp = pd.DataFrame(
+                full_table, columns=["date", "confirmed", "deaths", "recovered"]
+            )
+            st.write(f"Total reported cases for {selected_country}")
+
+        temp = temp.rename(columns={"date": "index"}).set_index("index")
+
         st.line_chart(temp)
+
     else:
-        min_date = datetime.datetime(2020,1,1)
-        max_date = datetime.date(2022,1,1)
 
-        selected_date = st.date_input("Pick a date", min_value=min_date, max_value=max_date)
+        mim_date_found = full_table["date"].min()
+        max_date_found = full_table["date"].max()
+
+        selected_date = st.date_input(
+            "Pick a date",
+            min_value=mim_date_found,
+            max_value=max_date_found,
+            value=max_date_found,
+        )
         "The date selected:", selected_date
-        full_table = full_table[full_table['date']== selected_date]
-        temp = pd.DataFrame(full_table, columns=['location','lat', 'lon','confirmed'])
-        temp =  temp.reset_index()
+        full_table = full_table[full_table["date"] == selected_date]
+        temp = pd.DataFrame(full_table, columns=["location", "lat", "lon", "confirmed"])
+        temp = temp.reset_index()
         st.write(temp)
-        st.write(temp['confirmed'].max())
-
-        
+        # st.write(temp["confirmed"].max())
 
         INITIAL_VIEW_STATE = pdk.ViewState(
-             latitude=55.3781,
-             longitude=-3.436,
-             zoom=1,
-             pitch=25,
+            latitude=55.3781,
+            longitude=-3.436,
+            zoom=1,
+            pitch=25,
         )
 
         # view = pdk.data_utils.compute_view(temp[["lon","lat"]])
         column_layer = pdk.Layer(
-                "ColumnLayer",
-                data=temp,
-                get_position=["lon", "lat"],
-                radius=50000,
-                get_elevation="confirmed",
-                elevation_scale=.70,
-                get_fill_color = ["255,255, confirmed*.1"],
-                get_line_color = [255,45,255],
-                filled = True,
-                pickable=True,
-                extruded=True,
-                auto_highlight=True,          
+            "ColumnLayer",
+            data=temp,
+            get_position=["lon", "lat"],
+            radius=50000,
+            get_elevation="confirmed",
+            elevation_scale=0.70,
+            get_fill_color=["255,255, confirmed*.1"],
+            get_line_color=[255, 45, 255],
+            filled=True,
+            pickable=True,
+            extruded=True,
+            auto_highlight=True,
         )
         TOOLTIP = {
-            "html": "<b>{confirmed}</b> Confirmed Cases <br>{location}",
-            "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
+            "html": "{location}<br> <b>{confirmed}</b> Confirmed Cases",
+            "style": {
+                "background": "grey",
+                "color": "white",
+                "font-family": '"Helvetica Neue", Arial',
+                "z-index": "10000",
+            },
         }
 
         r = pdk.Deck(
@@ -182,11 +266,12 @@ def main():
             # map_style=pdk.map_styles.SATELLITE,
             map_style="mapbox://styles/mapbox/light-v9",
             map_provider="mapbox",
-            initial_view_state = INITIAL_VIEW_STATE,
-            tooltip= TOOLTIP
+            initial_view_state=INITIAL_VIEW_STATE,
+            tooltip=TOOLTIP,
         )
+        st.write("## Total Number of Confirmed Cases All Time")
         st.pydeck_chart(r)
-        # r.to_html("test.html", open_browser=True, notebook_display=False)
+
 
 if __name__ == "__main__":
     main()
